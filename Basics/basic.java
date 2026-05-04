4a22b681469ca15b4bd5c7eb3bb57c9dbfa9e656
Jasim-10 <mdjasimansari010@gmail.com>
2026-05-04 23:33:42 +0530

    Java

--- Basics/basic.java
+++ Basics/basic.java
@@ -381,9 +381,12 @@
 
 
 //Switch Statement
+import java.util.Scanner;
 public class Basic {
     public static void main(String[] args) {
-        int day = 2;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the day: ");
+        int day = sc.nextInt();
         switch (day) {
             case 1:
                 System.out.println("Monday");
@@ -394,8 +397,24 @@ public class Basic {
         case 3:
             System.out.println("Wednesday");
             break;
+        case 4:
+            System.out.println("Thursday");
+            break;
+        case 5:
+            System.out.println("Friday");
+            break;
+        case 6:
+            System.out.println("Saturday");
+            break;
+        case 7:
+            System.out.println("Sunday");
+            break;
         default:
             System.out.println("Invalid day");
         }
     }
 }
+
+
+
+
