948354b969619e97c02b978a1a1cdd03aec4c8cc
Jasim-10 <mdjasimansari010@gmail.com>
2026-05-29 23:27:25 +0530

    Java

--- 01_practice/practice.java
+++ 01_practice/practice.java
@@ -81,15 +81,15 @@
 
 
 
-// import java.util.Scanner;
-// public class practice {
-//     public static void main(String[] args) {
-//         Scanner sc = new Scanner(System.in);
-//         System.out.print("Enter the number: ");
-//         int num = sc.nextInt();
-//         System.out.println("Number is: "+ num);
-//     }
-// }
+import java.util.Scanner;
+public class practice {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int num = sc.nextInt();
+        System.out.println("Number is: "+ num);
+    }
+}
 
 
 
