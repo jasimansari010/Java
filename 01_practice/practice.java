77d548cfd664322f7a50eb71592818c1ae4be9dc
Jasim-10 <mdjasimansari010@gmail.com>
2026-05-31 23:52:47 +0530

    Java

--- 01_practice/practice.java
+++ 01_practice/practice.java
@@ -71,13 +71,13 @@
 // }
 
 
-public class practice {
-    public static void main(String[] args) {
-        char ch = 'A';
-        int ascii = ch;
-        System.out.println("ASCII value of " + ch + " is " + ascii);
-    }
-}
+// public class practice {
+//     public static void main(String[] args) {
+//         char ch = 'A';
+//         int ascii = ch;
+//         System.out.println("ASCII value of " + ch + " is " + ascii);
+//     }
+// }
 
 
 
